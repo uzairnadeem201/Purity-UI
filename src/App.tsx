bea1ed86770f9eff1a8a0b95eec9665f3fdbf814
Uzi <m1pro@M1s-MacBook-Pro.local>
2025-09-10 19:40:51 +0500

    completed the setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import { Button, HStack } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { Provider } from "@/components/ui/provider"
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Provider>
+       <App />
+    </Provider>
+   
+  </StrictMode>,
+)
